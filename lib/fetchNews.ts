17eddd9e377563fa3c32a0fae391eb2f7a629d88
alikutluca <alikutluca@yahoo.com>
2022-12-14 16:56:36 +0300

    feat: header implemented

---
+++ lib/fetchNews.ts
@@ -0,0 +1,12 @@
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  // GraphQl query
+  // fetch function with Next.js 13 cahing...
+  //Sort function by images present
+  // retunr res
+};
+
+export default fetchNews;
