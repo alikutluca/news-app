1383b223ed171a3e95ae9c167feb2420bd9e7b20
alikutluca <alikutluca@yahoo.com>
2022-12-14 17:26:31 +0300

    feat: stepzen schema

--- app/page.tsx
+++ app/page.tsx
@@ -8,3 +8,6 @@ async function HomePage() {
 }
 
 export default HomePage;
+
+// for creating schema example import
+//stepzen import curl "http://api.mediastack.com/v1/news?access_key=66c655c23f321987b9f616b2f8783f12&countries=us%2Cgb&limit=100&offset=0&sort=published_desc"
